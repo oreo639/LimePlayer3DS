24eca936c13ba8a09f1cec6f99b64f4274e9a4ad
Oreo639 <31916379+Oreo639@users.noreply.github.com>
2019-02-20 10:25:54 -0800

    Update prepare.sh

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -5,15 +5,19 @@ mkdir -p ~/.local/ && mkdir -p ~/.local/bin
 source /etc/profile.d/devkit-env.sh
 export PATH=$PATH:~/.local/bin
 if ! [ -x "$(command -v bannertool)" ]; then
-  echo 'Error: bannertool is not installed. ' && echo 'Installing!'
+  echo '=> Bannertool is not installed. ' && echo '=> Installing!'
   git clone --recursive https://github.com/Steveice10/bannertool && cd bannertool/
   make && cp output/linux-x86_64/bannertool ~/.local/bin/bannertool
   cd ../ && rm -rf bannertool/
+else
+  echo '=> Bannertool is already installed.'
 fi
 if ! [ -x "$(command -v makerom)" ]; then
-  echo 'Error: makerom is not installed. ' && echo 'Installing!'
+  echo '=> Makerom is not installed. ' && echo '=> Installing!'
   git clone https://github.com/profi200/Project_CTR.git && cd Project_CTR/makerom
   make && cp makerom ~/.local/bin/makerom
   cd ../.. && rm -rf Project_CTR
+else
+  echo '=> Makerom is already installed.'
 fi
 echo 'Done!'
