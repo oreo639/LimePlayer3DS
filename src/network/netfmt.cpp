cae23f6b1187b96079a7836e4d1516ac83fcba55
Oreo639 <oreo639@outlook.com>
2019-05-26 15:59:07 -0700

    Fix license header

--- src/network/netfmt.cpp
+++ src/network/netfmt.cpp
@@ -1,9 +1,9 @@
-/*   LimePlayer3DS OSS graphcal music player for the Nintendo 3DS.
+/*   LimePlayer3DS FOSS graphcal music player for the Nintendo 3DS.
 *    Copyright (C) 2018-2019  LimePlayer Team
 *
 *    This program is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 2 of the License, or
+*    the Free Software Foundation, either version 3 of the License, or
 *    (at your option) any later version.
 *
 *    This program is distributed in the hope that it will be useful,
@@ -11,9 +11,8 @@
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *    GNU General Public License for more details.
 *
-*    YoYou should have received a copy of the GNU General Public License along
-*    with this program; if not, write to the Free Software Foundation, Inc.,
-*    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <stdio.h>
 #include <stdlib.h>
