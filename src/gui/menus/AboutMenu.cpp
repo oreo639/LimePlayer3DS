cae79105ae357f00baa3bef814f409cb905b5df1
Jaiden Nowak <windowsxp519@gmail.com>
2020-01-08 08:06:07 -0500

    Added build option

--- src/gui/menus/AboutMenu.cpp
+++ src/gui/menus/AboutMenu.cpp
@@ -14,6 +14,7 @@ void AboutMenu::drawTop() const
 	Gui::Print("About:", 20.0f, 20.0f, 0.5f, 0.5f);
 	Gui::Print("LimePlayer3DS version " LIMEPLAYER_VERSION, 40.0f, 40.0f, 0.5f, 0.5f);
 	Gui::Print("This application is licensed under GPL3.0-or-later.", 40.0f, 60.0f, 0.5f, 0.5f);
+	Gui::Print("Build" BUILD "(Beta)", 40.0f, 50.0f, 0.5f);
 }
 
 void AboutMenu::drawBottom() const
